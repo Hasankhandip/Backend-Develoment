122ce5cd80fc4ec889532fdd1facfd267b8bffb2
Dip <hasankhandip@gmail.com>
2023-06-09 21:30:16 +0600

    php 2.9

---
+++ php 2/php 2.9/test.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+$name = "Earth"; // global scope
+
+function dosomething(){
+    global $name;
+    echo $name;
+    
+    echo $GLOBALS ['name'];
+}
+
+dosomething();
+*/
+
+/*
+function dosomething(){
+    global $name;
+    $name = "Earth"; // local scope
+}
+
+dosomething();
+echo $name;
+*/
+
+
+/*
+function doSomething(){
+    $i = $i ?? 0;
+    $i++;
+    echo $i;
+    echo "\n";
+}
+
+doSomething();
+doSomething();
+*/
+
+
+function doSomething(){
+    static $i; //static scope
+    $i = $i ?? 0;
+    $i++;
+    echo $i;
+    echo "\n";
+}
+
+function doExtra(){
+    static $i;
+    $i = $i ?? 0;
+    $i++;
+    echo $i;
+    echo "\n";
+}
+
+doSomething();
+doSomething();
+doSomething();
+doSomething();
+doExtra();
+doExtra();
+doSomething();
+doExtra();
